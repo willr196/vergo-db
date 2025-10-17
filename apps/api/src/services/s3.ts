f0867c3c0989a4b7620c43eb0a62c26342fcf7b4
willr196 <willrobb11@gmail.com>
2025-10-17 19:38:50 +0100

    pretty much finished, need to finalise email situation and add a few headers

--- apps/api/src/services/s3.ts
+++ apps/api/src/services/s3.ts
@@ -1,6 +1,6 @@
-import { S3Client, PutObjectCommand, GetObjectCommand } from '@aws-sdk/client-s3''
-import { getSignedUrl } from '@aws-sdk/s3-request-presigner''
-import { env } from '../env''
+import { S3Client, PutObjectCommand, GetObjectCommand } from '@aws-sdk/client-s3'
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
+import { env } from '../env'
 
 const s3 = new S3Client({ region: env.s3Region })
 
